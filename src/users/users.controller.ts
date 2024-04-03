35cc00e82b92cfe9c05b245716c38500cecc2c4f
Keven Silva <keven.sp@hotmail.com>
2024-04-02 21:09:08 -0300

    Create CRUD

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Post()
+    async create(@Body() body) {
+        return {body};
+    }
+
+    @Get()
+    async read() {
+        return {users:[]}
+    }
+
+    @Get(':id')
+    async readOne(@Param() params) {
+        return {users:[], params}
+    }
+
+    @Put(':id')
+    async update(@Body() body, @Param() params) {
+        return {
+            body, 
+            params
+        }
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() params) {
+        return {
+            body, 
+            params 
+        }
+    }
+
+    @Delete(':id')
+    async delete(@Param() params) {
+        return {
+            params
+        }
+    }
+}
